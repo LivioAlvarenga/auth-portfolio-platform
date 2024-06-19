fd32ff48e97a9d82ff6b3850f142edd624d01f37
Livio Alvarenga <livioalvarenga@gmail.com>
2024-06-19 14:46:52 -0300

    chore: Add initial project files and configurations

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>adm.produtivese.com.br</h1>
+    </main>
+  );
+}
