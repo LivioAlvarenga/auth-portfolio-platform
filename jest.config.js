b5cf5a9ebf7edd7145ff0b12d9298bb1900e5330
Livio Alvarenga <livioalvarenga@gmail.com>
2024-07-09 09:57:58 -0300

    config jest with typescript

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+const jestConfig = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'node'],
+}
+
+module.exports = jestConfig
