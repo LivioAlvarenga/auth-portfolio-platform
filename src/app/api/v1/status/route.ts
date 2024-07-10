9b0636c881f6bd8c088660bc93bafb585015f513
Livio Alvarenga <livioalvarenga@gmail.com>
2024-07-10 11:58:04 -0300

    chore: Update status endpoint to include database information

--- src/app/api/v1/status/route.ts
+++ src/app/api/v1/status/route.ts
@@ -1,11 +1,40 @@
-import { query } from '@/infra/database'
+import { database } from '@/infra/database'
 import { NextResponse, type NextRequest } from 'next/server'
 
 async function status(req: NextRequest) {
   if (req.method === 'GET') {
-    const result = await query('SELECT 1 + 1;')
+    const updatedAt = new Date().toISOString()
 
-    return NextResponse.json({ message: 'ok' }, { status: 200 })
+    const databaseVersionResult = await database.query('SHOW server_version;')
+    const databaseVersionValue = databaseVersionResult.rows[0].server_version
+
+    const databaseConnectionsResult = await database.query(
+      'SHOW max_connections;',
+    )
+    const databaseConnectionsValue =
+      databaseConnectionsResult.rows[0].max_connections
+
+    const dataBaseName = process.env.POSTGRES_DB
+    const databaseOpenedConnectionsResult = await database.query({
+      text: 'SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;',
+      values: [dataBaseName],
+    })
+    const databaseOpenedConnectionsValue =
+      databaseOpenedConnectionsResult.rows[0].count
+
+    return NextResponse.json(
+      {
+        updated_at: updatedAt,
+        dependencies: {
+          database: {
+            version: databaseVersionValue,
+            max_connections: parseInt(databaseConnectionsValue),
+            oppened_connetions: databaseOpenedConnectionsValue,
+          },
+        },
+      },
+      { status: 200 },
+    )
   } else {
     return NextResponse.json({ message: 'method not allowed' }, { status: 405 })
   }
