5689b3beb0fd6c016b851be3df794da1271205d3
Livio Alvarenga <livioalvarenga@gmail.com>
2024-07-17 12:28:22 -0300

    Adds components `SVG` Assets `Icon`, `LogoHorizontal` and `LogoVertical`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,7 +50,7 @@ export default function RootLayout({
         <meta name="msapplication-TileColor" content="#ffffff" />
       </head>
       <body
-        className={`${sans400.variable} ${sans700.variable} ${sans500.variable} min-h-screen overflow-x-hidden scroll-smooth bg-background font-sans400 text-[16px] leading-[24px] tracking-[0.5px] text-foreground antialiased selection:bg-primary selection:text-foreground`}
+        className={`${sans400.variable} ${sans700.variable} ${sans500.variable} min-h-screen overflow-x-hidden scroll-smooth bg-background font-sans400 text-[16px] leading-[24px] tracking-[0.5px] text-foreground antialiased selection:bg-primary selection:text-primary-foreground`}
       >
         <AppProviders>
           <div className="relative flex min-h-screen flex-col bg-background">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
 import { ButtonThemeToggle } from '@/components/ButtonThemeToggle'
+import { Icon } from '@/components/svg/icon'
+import { LogoHorizontal } from '@/components/svg/logo-horizontal'
+import { LogoVertical } from '@/components/svg/logo-vertical'
 import { Text } from '@/components/Text'
 import { Button } from '@/components/ui/button'
 import { HomeIcon } from 'lucide-react'
@@ -14,29 +17,38 @@ export default function Home() {
   ] as const
   const buttonSizes = ['sm', 'default', 'lg', 'icon'] as const
   return (
-    <div className="my-10 overflow-hidden py-2 wrapper">
-      <ButtonThemeToggle />
-      <Text as="h1" variant="advertence-32-72-700">
-        adm.produtivese.com.br
-      </Text>
-      {buttonVariantsList.map((variant) =>
-        buttonSizes.map((size) => (
-          <div
-            key={`${variant}-${size}`}
-            className="flex flex-col items-center"
-          >
-            <div className="flex items-center justify-center gap-4 p-10">
-              <Button variant={variant} size={size}>
-                {size === 'icon' ? <HomeIcon /> : `${variant} - ${size}`}
-              </Button>
-              <Text
-                as="p"
-                variant="body-16-16-400"
-              >{`Variant: ${variant}, Size: ${size}`}</Text>
+    <>
+      <header className="border-b border-border shadow-sm">
+        <header className="flex h-28 w-full items-center justify-between wrapper">
+          <Icon />
+          <LogoHorizontal ariaLabel="Logo horizontal Produtivese" />
+          <ButtonThemeToggle />
+        </header>
+      </header>
+      <div className="my-10 overflow-hidden py-2 wrapper">
+        <LogoVertical ariaLabel="Logo vertical Produtivese" />
+        <Text as="h1" variant="advertence-32-72-700">
+          adm.produtivese.com.br
+        </Text>
+        {buttonVariantsList.map((variant) =>
+          buttonSizes.map((size) => (
+            <div
+              key={`${variant}-${size}`}
+              className="flex flex-col items-center"
+            >
+              <div className="flex items-center justify-center gap-4 p-10">
+                <Button variant={variant} size={size}>
+                  {size === 'icon' ? <HomeIcon /> : `${variant} - ${size}`}
+                </Button>
+                <Text
+                  as="p"
+                  variant="body-16-16-400"
+                >{`Variant: ${variant}, Size: ${size}`}</Text>
+              </div>
             </div>
-          </div>
-        )),
-      )}
-    </div>
+          )),
+        )}
+      </div>
+    </>
   )
 }
