b41f4eab485b5625a42aa719215b7919d9397827
Livio Alvarenga <livioalvarenga@gmail.com>
2024-07-17 11:16:59 -0300

    add `shadcn/ui` and button with all variants to test designer system colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,40 @@
 import { Text } from '@/components/Text'
+import { Button } from '@/components/ui/button'
+import { HomeIcon } from 'lucide-react'
 
 export default function Home() {
+  const buttonVariantsList = [
+    'default',
+    'destructive',
+    'outline',
+    'secondary',
+    'ghost',
+    'link',
+  ] as const
+  const buttonSizes = ['sm', 'default', 'lg', 'icon'] as const
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Text as="h1" variant="advertence-32-72-700">
         adm.produtivese.com.br
       </Text>
+      {buttonVariantsList.map((variant) =>
+        buttonSizes.map((size) => (
+          <div
+            key={`${variant}-${size}`}
+            className="flex flex-col items-center"
+          >
+            <div className="flex items-center justify-center gap-4 p-10">
+              <Button variant={variant} size={size}>
+                {size === 'icon' ? <HomeIcon /> : `${variant} - ${size}`}
+              </Button>
+              <Text
+                as="p"
+                variant="body-16-16-400"
+              >{`Variant: ${variant}, Size: ${size}`}</Text>
+            </div>
+          </div>
+        )),
+      )}
     </main>
   )
 }
