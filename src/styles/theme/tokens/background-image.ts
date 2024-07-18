abfc89fc53178e1c18179477a03bf92b6a59d3ee
Livio Alvarenga <livioalvarenga@gmail.com>
2024-07-18 14:41:14 -0300

    add background image to login

--- src/styles/theme/tokens/background-image.ts
+++ src/styles/theme/tokens/background-image.ts
@@ -1,1 +1,3 @@
-export const backgroundImage = {}
+export const backgroundImage = {
+  hero: `url('https://chpfldfxmaovtlouzcwg.supabase.co/storage/v1/object/public/assets-public/images/imagem-abstrata-tecnologia-produtividade-azul-branco.webp')`,
+}
