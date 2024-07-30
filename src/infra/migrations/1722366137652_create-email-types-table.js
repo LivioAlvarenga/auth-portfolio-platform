b31fec999385b792ee6f2d32a1b3166b87b8b6e9
Livio Alvarenga <livioalvarenga@gmail.com>
2024-07-30 16:34:56 -0300

    create `email_types` table

---
+++ src/infra/migrations/1722366137652_create-email-types-table.js
@@ -0,0 +1,38 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable('email_types', {
+    id: {
+      type: 'uuid',
+      default: pgm.func('gen_random_uuid()'),
+      notNull: true,
+      primaryKey: true,
+    },
+    type: {
+      type: 'varchar(255)',
+      notNull: true,
+      unique: true,
+    },
+    description: {
+      type: 'text',
+      notNull: false,
+    },
+  })
+}
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable('email_types')
+}
