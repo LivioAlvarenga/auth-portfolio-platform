ca904b5075476030fb0f05c2278ac295fdff9be6
Livio Alvarenga <livioalvarenga@gmail.com>
2024-07-30 11:19:38 -0300

    add backgroundImage to verify-email page

--- src/styles/theme/tokens/background-image.ts
+++ src/styles/theme/tokens/background-image.ts
@@ -1,4 +1,6 @@
 export const backgroundImage = {
   loginImage: `url('https://chpfldfxmaovtlouzcwg.supabase.co/storage/v1/object/public/assets-public/images/login-left-container-image.svg')`,
   registerImage: `url('https://chpfldfxmaovtlouzcwg.supabase.co/storage/v1/object/public/assets-public/images/register-left-container-image.svg')`,
+  verifyEmailImage: `url('https://chpfldfxmaovtlouzcwg.supabase.co/storage/v1/object/public/assets-public/images/verify-email-left-container-image.svg')`,
+  verifyEmailImageErro: `url('https://chpfldfxmaovtlouzcwg.supabase.co/storage/v1/object/public/assets-public/images/verify-email-left-container-image-error.svg')`,
 }
