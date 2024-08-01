ef95a1855d5caea6f27ca0f3a1570805185ef781
Livio Alvarenga <livioalvarenga@gmail.com>
2024-08-01 16:19:42 -0300

    fix: cors - add cors to all url of Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,34 @@ const nextConfig = {
       },
     ],
   },
+  // Configuração de cabeçalhos para todas as rotas API
+  async headers() {
+    return [
+      {
+        // Define que estas configurações se aplicam a todas as rotas que começam com /api/
+        source: '/api/:path*',
+        headers: [
+          {
+            // Especifica quais origens têm permissão para acessar os recursos.
+            // Aqui, usamos um wildcard (*) para permitir qualquer subdomínio no domínio vercel.app.
+            // Isso é útil para permitir pré-visualizações dinâmicas geradas pelo Vercel, onde cada branch pode ter seu próprio subdomínio.
+            key: 'Access-Control-Allow-Origin',
+            value: '', // Deixe vazio, pois será definido dinamicamente no middleware
+          },
+          {
+            // Define os métodos HTTP permitidos para acessar as rotas API.
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            // Especifica os cabeçalhos HTTP permitidos nas requisições.
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig
