a1877a339189d9cb7051ea0d12b1fd43d8abb34c
Livio Alvarenga <livioalvarenga@gmail.com>
2024-08-01 15:54:51 -0300

    fix: getBaseUrl - fix base url to work in dev, preview and production

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,7 +1,21 @@
 export function getBaseUrl() {
-  return process.env.VERCEL_ENV === 'production'
-    ? `https://adm.produtivese.com.br`
-    : process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
-      : `http://localhost:3000`
+  if (typeof window !== 'undefined') {
+    // Estamos no frontend
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'production') {
+      return `https://adm.produtivese.com.br`
+    }
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
+      return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    }
+    return `http://localhost:3000`
+  }
+
+  // Estamos no backend
+  if (process.env.VERCEL_ENV === 'production') {
+    return `https://adm.produtivese.com.br`
+  }
+  if (process.env.VERCEL_ENV === 'preview') {
+    return `https://${process.env.VERCEL_URL}`
+  }
+  return `http://localhost:3000`
 }
