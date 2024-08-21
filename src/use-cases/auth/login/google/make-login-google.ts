e24f6d46cc5a7813bae7a825955ffe7a0af84e61
Livio Alvarenga <livioalvarenga@gmail.com>
2024-08-21 10:57:28 -0300

    refactor(login): remove `accountRepository` of `LoginGoogleUseCase`

--- src/use-cases/auth/login/google/make-login-google.ts
+++ src/use-cases/auth/login/google/make-login-google.ts
@@ -1,5 +1,4 @@
 import { NextCookieRepository } from '@/repositories/nextjs/next-cookie-repository'
-import { PgAccountRepository } from '@/repositories/pg/pg-account-repository'
 import { PgAvatarRepository } from '@/repositories/pg/pg-avatar-repository'
 import { PgSessionRepository } from '@/repositories/pg/pg-session-repository'
 import { PgUserRepository } from '@/repositories/pg/pg-user-repository'
@@ -9,7 +8,6 @@ import { LoginGoogleUseCase } from './login-google'
 export function makeLoginGoogleUseCase() {
   const userRepository = new PgUserRepository()
   const sessionRepository = new PgSessionRepository()
-  const accountRepository = new PgAccountRepository()
   const avatarRepository = new PgAvatarRepository()
   const cookieRepository = new NextCookieRepository()
   const imageRepository = new SupabaseImageRepository()
@@ -17,7 +15,6 @@ export function makeLoginGoogleUseCase() {
   const useCase = new LoginGoogleUseCase(
     userRepository,
     sessionRepository,
-    accountRepository,
     avatarRepository,
     cookieRepository,
     imageRepository,
