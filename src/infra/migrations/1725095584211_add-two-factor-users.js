0b7af4517136d50596bc5b937b7a214e5b4903b8
Livio Alvarenga <livioalvarenga@gmail.com>
2024-08-31 06:23:59 -0300

    feat: add migration `two_factor_enabled` column in `users` table

---
+++ src/infra/migrations/1725095584211_add-two-factor-users.js
@@ -0,0 +1,28 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.addColumn('users', {
+    two_factor_enabled: {
+      type: 'boolean',
+      notNull: true,
+      default: false,
+    },
+  })
+}
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropColumn('users', 'two_factor_enabled')
+}
